43788d7bab2e849317030c8ef8f7f68e5e39e80d
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-08 10:44:37 +0530

    Add files via upload

---
+++ DataTypeConversion(3).js
@@ -0,0 +1,19 @@
+// //convert Number to String
+
+let score = 50;
+console.log(typeof score);  //(typeof se check krte hai konsa datatype hai )
+
+let NewScore = String(score);
+console.log(typeof NewScore);
+
+//convert String to Number;
+let name = "Arman";
+console.log(typeof name);
+let newName = Number(name);
+console.log(typeof newName);
+
+
+
+
+
+
