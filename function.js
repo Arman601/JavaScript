2e99173360d60e4566dc6384837f6a99d354eb90
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-10 21:41:06 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+function myname() {
+    console.log("A")
+    console.log("R")
+    console.log("M")
+    console.log("A")
+    console.log("N")
+
+}
+// myname();
+
+//add two number
+function add(number1, number2) {
+    console.log(number1 + number2);
+}
+add(10, 20);
+
+
+
+
+
