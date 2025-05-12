e7ddcf10006be854a338f35a698010b8cfb21901
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-12 23:25:18 +0530

    Add files via upload

---
+++ For_Loop.js
@@ -0,0 +1,31 @@
+
+let a = ["Arman", "Rizwan ", "kaif"];
+for (let i = 0; i < a.length; i++) {
+    console.log("Name", a[i]);
+}
+
+
+
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        break;
+    }
+    // console.log(i);
+    console.log(`values:${i}`);
+}
+
+
+
+
+for (let index = 1; index <= 10; index++) {
+    if (index == 5) {
+        continue;
+    }
+    // console.log(index);
+    console.log(`index vlaue:${index}`);
+
+}
+
+
+
