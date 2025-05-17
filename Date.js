41e271cbead19bd8cddbbbe39dcac8aba515c921
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-17 22:16:14 +0530

    Add files via upload

---
+++ Date.js
@@ -0,0 +1,13 @@
+
+let date = new Date();
+console.log(date);
+console.log(date.toString());//convert in string for readable form 
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+console.log(typeof date);
+
+let mydate = new Date(2025, 0, 23);//khud se time set krna (month js me 0 se suru hota hai)
+console.log(mydate.toString());
+
+
+
