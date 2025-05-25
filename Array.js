a05e7a3ee7877512d2ffde98478eba8e90072da3
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-25 23:50:14 +0530

    Add files via upload

--- Array.js
+++ Array.js
@@ -1,25 +1,13 @@
-//1st way to defined Array
-let number = [10, 20, 30, 40]
-console.log(number);
-let Details = ["Arman", 55, "Nawada"];
-console.log(Details);
+// //1st way to defined Array
+// let number = [10, 20, 30, 40]
+// console.log(number);
+// let Details = ["Arman", 55, "Nawada"];
+// console.log(Details);
 
-//2nd way to defined Array
-let number2 = new Array(1, 2, 3, 4, 5);
-console.log(number2);
-number2.push(10); //insert element
-number2.pop();  //delete element
-console.log(number2);
-
-
-// number2.unshift(500);  //to add element in beginning
-// number2.shift() //to delete element from array
-
-
-
-
-
-
-
-
+// //2nd way to defined Array
+// let number2 = new Array(1, 2, 3, 4, 5);
+// console.log(number2);
+// number2.push(10); //insert element
+// number2.pop();  //delete element
+// console.log(number2);
 
