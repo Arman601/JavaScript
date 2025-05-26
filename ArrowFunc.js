4d649f6e6cf06e566ccb717e2e6213b86d45a640
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-26 23:15:22 +0530

    Add files via upload

---
+++ ArrowFunc.js
@@ -0,0 +1,13 @@
+// //Arraw function
+
+// // const addtwo = (num1, num2) => num1 + num2;
+// // console.log(addtwo(4, 4))
+
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+// console.log(addTwo(10, 10));
+
+
