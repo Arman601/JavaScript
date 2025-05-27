4c3fd2975f92d5b4789a96bca381dd51c518a039
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-05-27 22:26:33 +0530

    Add files via upload

---
+++ object02.js
@@ -0,0 +1,53 @@
+// // defined object using constructor
+// //
+// const insta_User = new Object();  //ye constructor se define hua(singleton object);
+// // const insta_User = {}   // non singleton object(literal)
+// console.log(insta_User);
+// insta_User.name = "Arman";
+// insta_User.id = 55;
+// console.log(insta_User);
+
+
+
+// const Regular_User = {
+//     email: "arman@gmail.com",
+//     fullname: {  //nexting
+//         userfulname: {
+//             firstname: "Arman",
+//             lastname: "Hasan"
+
+//         }
+
+
+
+//     }
+// }
+// console.log(Regular_User.fullname.userfulname);
+
+
+// const a = {
+//     address: "Nawada"
+// }
+// const b = {
+//     fulladdress: "Bihar"
+// }
+
+// const c = { ...a, ...b }; //spread(concanet)
+// console.log("fullAddress:", c);
+
+
+// //Destructure of object
+// const course = {
+//     courseName: "js in hindi",
+//     price: 999,
+//     courseInstructor: "Arman"
+// }
+
+// // const a2 = course.courseInstructor;
+// // console.log(a2);
+// const { courseInstructor } = course
+// console.log(courseInstructor);
+
+
+
+
