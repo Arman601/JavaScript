14e5dcd062746eae98fa5d79fdf3175e3c5d78c3
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-06-03 23:55:28 +0530

    Add files via upload

--- Book.js
+++ Book.js
@@ -1,12 +1,13 @@
-//
-let student = [
-    { name: "Arman", roll: 55, class: 5 },
-    { name: "Rizwan", roll: 20, class: 6 },
-    { name: "Arman", roll: 40, class: 7 },
-    { name: "Khan", roll: 50, class: 10 },
-    { name: "kaif", roll: 60, class: 10 }
+// let student = [
+//     { name: "Arman", roll: 55, class: 5 },
+//     { name: "Rizwan", roll: 20, class: 6 },
+//     { name: "Arman", roll: 40, class: 7 },
+//     { name: "Khan", roll: 50, class: 10 },
+//     { name: "kaif", roll: 60, class: 10 }
 
-]
+// ]
 
-let detailss = student.filter((any) => any.name == "Arman");
-console.log(detailss);
+// let detailss = student.filter((any) => any.name == "Arman");
+// console.log(detailss);
+
+
