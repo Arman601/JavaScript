23c915590e4f4b0988a05bc503d72f4da2ba1c46
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-06-04 23:03:46 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,22 @@
+const accountId = 12786  //is change  nhi kr skte hai
+let accountEmail = "arman@42gmail.com"
+var accountPassword = "12456"
+myCity = "Nawada"  //bina keywor ke bhi variable decelare ho skta hai
+let myPhoneNo; //koi value assign nhi krne pr undefined print hoga
+
+/*prefer not to use var( jaydatr let use krege humlog ) 
+because of issue in block scope and fuctional scope
+*/
+
+
+//table use krne se ek line me hi print hogya
+console.table([accountId, accountEmail, accountPassword, myCity]);
+
+// console.log("Id: ", accountId)
+// console.log("Email: ", accountEmail)
+// console.log("Password: ", accountPassword)
+// console.log("City: ", myCity)
+
+
+
+
