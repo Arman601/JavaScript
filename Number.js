0daed327e309afdef2ae92f1341e1231988ed074
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-06-05 22:17:36 +0530

    Add files via upload

---
+++ Number.js
@@ -0,0 +1,21 @@
+// //first way to print number
+// let a = 50;
+// console.log(a);
+
+// //second way
+// let a2 = new Number(100);
+// console.log(a2);
+// console.log(a2.toString());  //number to String me convert
+
+// //MATH 
+
+// console.log(Math.min(3, 30, 4, 77, 2));
+// console.log(Math.max(3, 30, 4, 77, 2));
+// console.log(Math.random()); //ye zero or one ke bech me value generate krti hai
+// console.log(Math.floor(Math.random() * 10 + 1)); // 1 to 10 tk genereate hoga 
+
+// let num = new String("Arman");
+
+
+
+
