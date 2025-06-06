464728971fe55089ac92e8eefbdaaca26fc7c5bd
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-06-06 23:47:15 +0530

    Add files via upload

---
+++ while_dowhile_Loop.js
@@ -0,0 +1,30 @@
+//while Loop
+let i = 2;
+while (i <= 20) {
+    console.log(i);
+    // if (i == 5) {
+    //     continue;
+    // } 
+
+    i = i + 2;
+
+}
+
+// let hero = ["salman ", "shahrukh ", "ajay"];
+// let a = 0;
+// while (a < hero.length) {
+//     console.log(hero[a]);
+//     a++;
+// }
+
+
+
+// //do_while_Loop
+
+// let a = 2;
+// do {
+//     console.log(a);
+//     a = a + 2;
+// }
+// while (a <= 10);
+
